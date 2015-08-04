bc09524e57db29716159f6488ef65f312a967edc
Tanmayee Takalkar <tanmayee21193@gmail.com>
2015-08-04 12:33:30 +0530

    modified the previous helloworld file for supermarket functionality

--- src/test/java/junit/controller/ProductControllerSpec.java
+++ src/test/java/junit/controller/ProductControllerSpec.java
@@ -1,8 +1,10 @@
 package junit.controller;
 
-import hello.ProductController;
+import controller.ProductController;
 import org.junit.Assert;
 import org.junit.Test;
+
+import java.util.List;
 
 /**
  * Created by Yashawant and Tanmayee on 28/7/15.
@@ -11,7 +13,7 @@ public class ProductControllerSpec {
     @Test
     public void it_shouldReturnHello(){
         ProductController productController = new ProductController();
-        String actual = productController.getProductList();
+        List actual = productController.getProductList();
        // Assert.assertEquals("cartName1 Not Available", actual);
     }
 }
