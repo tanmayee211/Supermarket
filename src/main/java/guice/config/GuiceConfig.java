1baea4caa3ae9471c7ccf34b9d6b4d26fe5c39c1
Tanmayee Takalkar <tanmayee21193@gmail.com>
2015-08-11 11:21:37 +0530

    configuration files for GuiceConfig, appConfig for test and main.

---
+++ src/main/java/guice/config/GuiceConfig.java
@@ -0,0 +1,36 @@
+package guice.config;
+
+import com.google.inject.Guice;
+import com.google.inject.Injector;
+import com.google.inject.servlet.GuiceServletContextListener;
+import com.sun.jersey.api.core.PackagesResourceConfig;
+import com.sun.jersey.guice.JerseyServletModule;
+import com.sun.jersey.guice.spi.container.servlet.GuiceContainer;
+import dao.MorphiaConfig;
+import dao.ProductDao;
+import resource.ProductResource;
+
+import java.util.HashMap;
+
+/**
+ * Created by user on 10/8/15.
+ */
+public class GuiceConfig extends GuiceServletContextListener {
+
+    @Override
+    protected Injector getInjector() {
+
+        return Guice.createInjector(new JerseyServletModule() {
+
+            @Override
+            protected void configureServlets() {
+                bind(ProductResource.class);
+                bind(ProductDao.class);
+                bind(MorphiaConfig.class);
+                HashMap<String, String> options = new HashMap<>();
+                options.put("com.sun.jersey.api.json.POJOMappingFeature", "true");
+                serve("/rest/*").with(GuiceContainer.class ,options);
+            }
+        });
+    }
+}
